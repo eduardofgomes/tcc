4e284c3e8bbdc8f0e56def38959b0481bf913194
eduardofgomes <eduardofranciscogomes01@gmail.com>
2022-06-02 07:49:01 -0300

    02.06

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -5,8 +5,9 @@
     $username = "root";
     $password = "";
 
-    if($conecta = mysqli_connect($hostname, $username, $password, $database)){
-        echo 'Conectado ao banco de dados '.$database.'.....';
-    } else {
-        echo 'Erro: '.mysqli_connect_error();
+    try{
+        $pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo 'Error: '.$e->getMessage();
     }

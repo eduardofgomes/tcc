53ddfdb96b8f71c41c21e4f0848d533ed371392b
lucaspedrozo <lucasdasilvapedrozo12121@gmail.com>
2022-08-17 14:43:42 -0300

    17.08

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -8,6 +8,7 @@
     try{
         $pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname, $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conexão com o banco de dados realizada com sucesso";
     } catch (PDOException $e) {
         echo 'Error: '.$e->getMessage();
     }

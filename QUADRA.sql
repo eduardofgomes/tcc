1b8cee5407fed3aba7bc6a661c4009b5018582ea
Brunohploureiro <brunohploureiro@gmail.com>
2022-09-08 09:25:11 -0300

    08.09

--- QUADRA.sql
+++ QUADRA.sql
@@ -1,60 +1,180 @@
--- -----------------------------------------------------
--- Table `quadra`.`USUARIO`
--- -----------------------------------------------------
-CREATE TABLE `USUARIO` (
-  `LOGIN` VARCHAR(45) NOT NULL,
-  `SENHA` VARCHAR(45) NOT NULL,
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `EMAIL` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`ID`))
-ENGINE = InnoDB;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 08-Set-2022 às 14:23
+-- Versão do servidor: 10.4.20-MariaDB
+-- versão do PHP: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- -----------------------------------------------------
--- Table `quadra`.`CIDADAO`
--- -----------------------------------------------------
-CREATE TABLE `CIDADAO` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `NOME` VARCHAR(45) NOT NULL,
-  `EMAIL` VARCHAR(45) NOT NULL,
-  `FOTO` MEDIUMBLOB NOT NULL,
-  `SENHA` VARCHAR(30) NOT NULL,
-  `RG` VARCHAR(9) NOT NULL,
-  `USUARIO_ID` INT NOT NULL,
-  PRIMARY KEY (`ID`))
-ENGINE = InnoDB;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Banco de dados: `quadra`
+--
 
--- -----------------------------------------------------
--- Table `quadra`.`TIPO_USUARIO`
--- -----------------------------------------------------
-CREATE  TABLE `TIPO_USUARIO` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `NOME` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`ID`))
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Estrutura da tabela `cidadao`
+--
 
--- -----------------------------------------------------
--- Table `quadra`.`RESERVAS`
--- -----------------------------------------------------
-CREATE  TABLE `RESERVAS` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `DATA` DATETIME(6) NOT NULL,
-  `USUARIO_ID` INT NOT NULL,
-  PRIMARY KEY (`ID`))
-ENGINE = InnoDB;
+CREATE TABLE `cidadao` (
+  `ID` int(11) NOT NULL,
+  `NOME` varchar(45) NOT NULL,
+  `EMAIL` varchar(45) NOT NULL,
+  `FOTO` mediumblob NOT NULL,
+  `SENHA` varchar(30) NOT NULL,
+  `RG` varchar(9) NOT NULL,
+  `USUARIO_ID` int(11) NOT NULL,
+  `TIPO_ID` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `quadra`.`QUADRAS`
--- -----------------------------------------------------
-CREATE  TABLE `QUADRAS` (
-  `ID` INT NOT NULL AUTO_INCREMENT,
-  `NUMERO` INT(6) NOT NULL,
-  `NOME` VARCHAR(45) NOT NULL,
-  `BAIRRO` VARCHAR(45) NOT NULL,
-  `LOGRADOURO` VARCHAR(45) NOT NULL,
-  `RESERVAS_ID` INT NOT NULL,
-  PRIMARY KEY (`ID`))
-ENGINE = InnoDB;
+--
+-- Estrutura da tabela `quadras`
+--
+
+CREATE TABLE `quadras` (
+  `ID` int(11) NOT NULL,
+  `NUMERO` int(6) NOT NULL,
+  `NOME` varchar(45) NOT NULL,
+  `BAIRRO` varchar(45) NOT NULL,
+  `LOGRADOURO` varchar(45) NOT NULL,
+  `RESERVAS_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `ID` int(11) NOT NULL,
+  `DATA` datetime(6) NOT NULL,
+  `USUARIO_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipo_usuario`
+--
+
+CREATE TABLE `tipo_usuario` (
+  `ID` int(5) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tipo_usuario`
+--
+
+INSERT INTO `tipo_usuario` (`ID`, `nome`) VALUES
+(1, 'Cidadão'),
+(2, 'ADM'),
+(3, 'Zelador');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `LOGIN` varchar(45) NOT NULL,
+  `SENHA` varchar(45) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `EMAIL` varchar(45) NOT NULL,
+  `tipo_id_user` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`LOGIN`, `SENHA`, `ID`, `EMAIL`, `tipo_id_user`) VALUES
+('totozo', '202cb962ac59075b964b07152d234b70', 1, 'natan@gmail.com', 3);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `cidadao`
+--
+ALTER TABLE `cidadao`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices para tabela `quadras`
+--
+ALTER TABLE `quadras`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices para tabela `reservas`
+--
+ALTER TABLE `reservas`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices para tabela `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cidadao`
+--
+ALTER TABLE `cidadao`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `quadras`
+--
+ALTER TABLE `quadras`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `reservas`
+--
+ALTER TABLE `reservas`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

1350f75a1c4d9880e88f824d2bbd7cf9b32a0c8f
brunohploureiro <brunohploureiro@gmail.com>
2022-09-29 09:09:39 -0300

    29.09

--- QUADRA.sql
+++ QUADRA.sql
@@ -9,7 +9,7 @@ CREATE TABLE `CIDADAO` (
   `SENHA` VARCHAR(30) NOT NULL,
   `RG` VARCHAR(9) NOT NULL,
   `USUARIO_ID` INT NOT NULL,
-  `TIPO_ID` NOT NULL,
+  `TIPO_ID`INT NOT NULL,
   PRIMARY KEY (`ID`))
 ENGINE = InnoDB;
 
@@ -58,22 +58,23 @@ INSERT INTO `TIPO_USUARIO` (`ID`, `NOME`) VALUES
 (2, 'ADM'),
 (3, 'Zelador');
 */
+
 -- --------------------------------------------------------
 
 --
 -- Estrutura da tabela `USUARIO`
 --
 
-/*
+
 CREATE TABLE `USUARIO` (
   `LOGIN` varchar(45) NOT NULL,
   `SENHA` varchar(45) NOT NULL,
   `ID` INT(11) NOT NULL,
   `EMAIL` varchar(45) NOT NULL,
-  `TIPO_ID_USER` INT(1) NOT NULL
+  `TIPO_ID` INT NOT NULL
 ) ENGINE=InnoDB;
-*/
 
+/*
 CREATE TABLE `USUARIO` (
   `LOGIN` VARCHAR(45) NOT NULL,
   `SENHA` VARCHAR(45) NOT NULL,
@@ -82,3 +83,4 @@ CREATE TABLE `USUARIO` (
   `TIPO_ID` INT NOT NULL,
   PRIMARY KEY (`ID`))
 ENGINE = InnoDB;
+*/

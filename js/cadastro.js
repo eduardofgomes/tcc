d1aade11d3c6cf72d920383e1afeadd021cc3eb5
Brunohploureiro <brunohploureiro@gmail.com>
2022-11-22 18:42:53 -0300

    22.11

---
+++ js/cadastro.js
@@ -0,0 +1,32 @@
+const cpf = document.querySelector("#CPF")
+
+cpf.addEventListener("keyup", event => {
+    let start = cpf.selectionStart 
+    let end = cpf.selectionEnd 
+    if(start == end) { 
+        formMask("___.___.___-__", "_", event, start);
+    }
+})
+
+function formMask(mask, char, event, cursor) {
+    
+    const vetMask = mask.split("") 
+    const onlyNumbers = cpf.value.split("").filter(value => !isNaN(value) && value != " ")
+    const key = event.keyCode || event.which
+    const backspaceAndArrowKeys = [8, 37, 38, 39, 40] 
+    const clickedOnTheBackspaceOrArrowsKeys = backspaceAndArrowKeys.indexOf(key) >= 0
+    const charNoMod = [".", "-"] 
+    const cursorIsCloseToCharNoMod = charNoMod.indexOf(vetMask[cursor]) >= 0
+
+    onlyNumbers.forEach( (value) => vetMask.splice(vetMask.indexOf(char), 1, value)) 
+
+    cpf.value = vetMask.join("")
+
+    if(!clickedOnTheBackspaceOrArrowsKeys && cursorIsCloseToCharNoMod) { 
+        cpf.setSelectionRange(cursor+1, cursor+1)
+    } else {
+        cpf.setSelectionRange(cursor, cursor)
+    }
+
+    //i used google translate, sorry :D
+}

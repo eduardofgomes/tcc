45c7eee91e16ef3613d5380f68edb58cf26b6c6d
eduardofgomes <eduardofranciscogomes01@gmail.com>
2022-12-04 17:22:56 -0300

    04.12

--- src/cidadao/controle/view-cidadao.js
+++ src/cidadao/controle/view-cidadao.js
@@ -22,16 +22,17 @@ $(document).ready(function() {
                     $('.modal-body').load('src/cidadao/visao/form-cidadao.html', function() {
                         $('#NOME').val(dado.dados.NOME)
                         $('#NOME').attr('readonly', 'true')
-                        $('#EMAIL').val(dado.dados.EMAIL)
+                        $('#EMAIL').val(dado.dados22.EMAIL)
                         $('#EMAIL').attr('readonly', 'true')
-                        $('#SENHA').val(dado.dados.SENHA)
+                        $('#SENHA').val(dado.dados2.SENHA)
                         $('#SENHA').attr('readonly', 'true')
                         $('#FOTO').val(dado.dados.FOTO)
                         $('#FOTO').attr('readonly', 'true')
                         $('#CPF').val(dado.dados.CPF)
                         $('#CPF').attr('readonly', 'true')
 
-                        //Consultar todos os tipos cadastrados no banco de daods
+                        //Consultar todos os tipos cadastrados no banco de dados
+                        /*
                         $.ajax({
                             dataType: 'json',
                             type: 'POST',
@@ -41,6 +42,7 @@ $(document).ready(function() {
                                 
                             }
                         })
+                        */
 
                     })
                     $('.btn-save').hide()
--- src/cidadao/modelo/view-cidadao.php
+++ src/cidadao/modelo/view-cidadao.php
@@ -3,11 +3,13 @@
     include('../../conexao/conn.php');
 
     $ID = $_REQUEST['ID'];
+    $USUARIO_ID = $_REQUEST['USUARIO_ID'];
 
     $sql = "SELECT * FROM CIDADAO WHERE ID = $ID";
-    $dados_usuario = "SELECT * FROM USUARIO"
+    $sql2 = "SELECT * FROM USUARIO WHERE ID = $USUARIO_ID";
 
     $resultado = $pdo->query($sql);
+    $resultado2 = $pdo->query($sql2);
 
     if($resultado){
         $result = array();
@@ -28,4 +30,24 @@
         );
     }
 
+    if($resultado2){
+        $result = array();
+        while($row2 = $resultado2->fetch(PDO::FETCH_ASSOC)){
+
+            $result2 = array_map('utf8_encode', $row2);
+        }
+        $dados2 = array(
+            'tipo' => 'success',
+            'mensagem' => '',
+            'dados' => $result2
+        );
+    } else {
+        $dados2 = array(
+            'tipo' => 'error',
+            'mensagem' => 'Não foi possível obter o registro solicitado.',
+            'dados' => array()
+        );
+    }
+
     echo json_encode($dados);
+    echo json_encode($dados2);

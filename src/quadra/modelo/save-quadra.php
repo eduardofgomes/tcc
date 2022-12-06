45e9d43102edede2eceff2279beaaa0b6531c1b1
eduardofgomes <eduardofranciscogomes01@gmail.com>
2022-12-06 19:03:06 -0300

    06.12

--- src/quadra/modelo/save-quadra.php
+++ src/quadra/modelo/save-quadra.php
@@ -1,58 +1,64 @@
 <?php
-                if(empty($requestData['NOME'])){
-                    // Caso a variável venha vazia eu gero um dados de erro do mesmo
-                    $dados = array(
-                        "tipo" => 'error',
-                        "mensagem" => 'Existe(m) campo(s) obrigatório(s) não preenchido(s).'
-                    );
-                } else {
-                    // Caso não exista campo em vazio, vamos gerar a requisição
-                    $ID = isset($requestData['ID']) ? $requestData['ID'] : '';
-                    $operacao = isset($requestData['operacao']) ? $requestData['operacao'] : '';
 
-                    // Verifica se é para cadastrar um novo registro
-                    if($operacao == 'insert'){
-                        // Prepara o comando INSERT para ser executado
-                        try{
-                            $TIPO_CIDADAO = '2';
-                            $stmt = $pdo->prepare('INSERT INTO QUADRA (NOME, NUMERO, BAIRRO, LOGRADOURO) VALUES (:a, :b, :c, :d, :e)');
-                            $stmt->execute(array(
-                                ':a' => $requestData['NOME'],
-                                ':b' => $requestData['NUMERO'],
-                                ':c' => $requestData['BAIRRO'],
-                                ':d' => $requestData['LOGRADOURO']                       
-                            ));
-                          
-                            
-                                    $stmt = $pdo->prepare('INSERT INTO QUADRA (NOME, NUMERO, BAIRRO, LOGRADOURO) VALUES (:a, :b, :c, :d)');
-                                    $stmt->execute(array(
-                                        ':a' => $requestData['NOME'],
-                                        ':b' => $requestData['NUMERO'],
-                                        ':c' => $requestData['BAIRRO'],
-                                        ':d' => $requestData['LOGRADOURO']
-                                    ));
+    // Obter a nossa conexão com o banco de dados
+    include('../../conexao/conn.php');
 
-                            $dados = array(
-                                "tipo" => 'success',
-                                "mensagem" => 'Quadra cadastrado com sucesso.'
-                            );
+    // Obter os dados enviados do formulário via $_REQUEST
+    $requestData = $_REQUEST;
 
-                   
-                            
-                        } catch(PDOException $e) {
-                            $dados = array(
-                                "tipo" => 'error',
-                                "mensagem" => 'Não foi possível criar a quadra.'
-                            );
-                        }
+    // Verificação de campo obrigatórios do formulário
+    if(empty($requestData['NOME'])){
+        // Caso a variável venha vazia eu gero um retorno de erro do mesmo
+        $dados = array(
+            "tipo" => 'error',
+            "mensagem" => 'Existe(m) campo(s) obrigatório(s) não preenchido(s).'
+        );
+    } else {
+        // Caso não exista campo em vazio, vamos gerar a requisição
+        $ID = isset($requestData['ID']) ? $requestData['ID'] : '';
+        $operacao = isset($requestData['operacao']) ? $requestData['operacao'] : '';
 
-                    } 
-        
-                } else {
-                    $dados = array ('mensagem' => 'Erro ao salvar o arquivo. Aparentemente você não tem permissão para editar essa área.');
-                }
-    
-        
-
-
-    echo json_encode($dados);
+        // Verifica se é para cadastrar um novo registro
+        if($operacao == 'insert'){
+            // Prepara o comando INSERT para ser executado
+            try{
+                $stmt = $pdo->prepare('INSERT INTO QUADRA (NOME, NUMERO, BAIRRO, LOGRADOURO) VALUES (:a, :b, :c, :d)');
+                $stmt->execute(array(
+                    ':a' => $requestData['NOME'],
+                    ':b' => $requestData['NUMERO'],
+                    ':c' => $requestData['BAIRRO'],
+                    ':d' => $requestData['LOGRADOURO']
+                ));
+                $dados = array(
+                    "tipo" => 'success',
+                    "mensagem" => 'Registro salvo com sucesso.'
+                );
+            } catch(PDOException $e) {
+                $dados = array(
+                    "tipo" => 'error',
+                    "mensagem" => 'Não foi possível efetuar o cadastro do usuario.'
+                );
+            }
+        } else {
+            // Se minha variável operação estiver vazia então devo gerar os scripts de update
+            try{
+                $stmt = $pdo->prepare('UPDATE QUADRA SET NOME = :a, NUMERO = :b, BAIRRO = :c, LOGRADOURO = :d WHERE ID = :id');
+                $stmt->execute(array(
+                    ':id' => $ID,
+                    ':a' => $requestData['NOME'],
+                    ':b' => $requestData['NUMERO'],
+                    ':c' => $requestData['BAIRRO'],
+                    ':d' => $requestData['LOGRADOURO']
+                ));
+                $dados = array(
+                    "tipo" => 'success',
+                    "mensagem" => 'Registro atualizado com sucesso.'
+                );
+            } catch (PDOException $e) {
+                $dados = array(
+                    "tipo" => 'error',
+                    "mensagem" => 'Não foi possível efetuar a alteração do registro.'
+                );
+            }
+        }
+    }

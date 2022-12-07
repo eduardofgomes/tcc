632d2c4b4c722b4db8b1580eeb94cabd6be0b254
eduardofgomes <eduardofranciscogomes01@gmail.com>
2022-12-07 17:56:59 -0300

    07.12

--- src/reserva/controle/new-reserva.js
+++ src/reserva/controle/new-reserva.js
@@ -8,7 +8,18 @@ $(document).ready(function() {
         $('.modal-title').append('Adicionar nova reserva')
 
         $('.modal-body').load('src/reserva/visao/form-reserva.html')
-
+        $('.modal-body').load('src/vendedor/visao/form-vendedor.html', function(){
+            $.ajax({
+            dataType: 'json',
+            type: 'POST',
+            assync: true,
+            url: 'src/tipo/modelo/all-tipo.php',
+            success: function(dados){
+                for(const result of dados){
+                    $('#TIPO_ID').append(`<option value="${result.ID}">${result.NOME}</option>`)
+                }
+            }  
+        })
         $('.btn-save').show()
         $('.btn-save').attr('data-operation', 'insert')
         $('#modal-reserva').modal('show')
